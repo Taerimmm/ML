b16e520fbe0b08f21d2eb7f55a693a8975670d5f
Taerimmm <xofla7560@naver.com>
2021-01-12 15:11:42 +0900

    Model

--- keras/keras35_2_load_model.py
+++ keras/keras35_2_load_model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import load_model
-model = load_model('./model/save_keras35.h5')
+model = load_model('../data/h5/save_keras35.h5')
 
 model.summary()
 
--- keras/keras35_4_load_model.py
+++ keras/keras35_4_load_model.py
@@ -2,7 +2,7 @@ import numpy as np
 a = np.array(range(1,11))
 
 from tensorflow.keras.models import load_model
-model = load_model('./model/save_keras35.h5')
+model = load_model('../data/h5/save_keras35.h5')
 # 요 밑 3줄 넣고 테스트 ######
 from tensorflow.keras.layers import Dense
 model.add(Dense(5, name='kingkeras1'))
