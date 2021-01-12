ffd3f958778d82b9b7b25db450cd00aeaf1c5642
Taerimmm <xofla7560@naver.com>
2021-01-12 09:38:00 +0900

    MC

--- keras/keras46_MC_3_cifar100.py
+++ keras/keras46_MC_3_cifar100.py
@@ -19,14 +19,17 @@ x_test = x_test/255.
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 model = Sequential()
-model.add(Conv2D(512, (2,2), padding='same', strides=1, input_shape=(32,32,3)))
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same', strides=1, input_shape=(32,32,3)))
+model.add(Conv2D(32, (3,3), activation='relu', padding='same', strides=1))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.2))
-model.add(Conv2D(256, (2,2), padding='same', strides=1))
+model.add(Conv2D(128, (3,3), activation='relu', padding='same', strides=1))
+model.add(Conv2D(128, (3,3), activation='relu', padding='same', strides=1))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.2))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(100, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
