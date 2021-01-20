40fbcaa96e12750f50f9a3ed3f1a2eb97ee41154
Taerimmm <xofla7560@naver.com>
2021-01-20 20:47:02 +0900

    Dacon_sunlight

---
+++ dacon/sunlight_model2.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+
+# 데이터
+data = pd.read_csv('./dacon/data/train/train.csv', header=0)
+
+data.set_index(['Day','Hour','Minute'], inplace=True)
+print(data.shape)
+print(data.head())
+
+a = []
+for i in range(len(data)):
+    target = data.iloc[i,-1]
+    if target > 0.0:
+        a.append(1)
+    else :
+        a.append(0)
+data['target_binary'] = a
+
+def split_x(data, x_row, x_col, y_row, y_col):
+    a, b =[], []
+    x_step, y_step = 0, 0
+    for i in range(data.shape[0] - x_row + 1):
+        x_step += 1
+        a.append(np.array(data.iloc[i:i+x_row,:x_col]))
+
+    for i in range(data.shape[0] - y_row + 1):
+        if x_row + y_row+ i > data.shape[0]:
+            break
+        y_step += 1
+        b.append(np.array(data.iloc[x_row+i:x_row+i+y_row,-y_col:]))
+
+    a = np.array(a)[:y_step]
+    b = np.array(b)
+
+    return  a, b
+
+x_row, x_col = 2*24*7, 1
+y_row, y_col = 2*24*2, 1
+(x_data, y_data) = split_x(data.iloc[:,-2:], x_row, x_col, y_row, y_col)
+print(x_data.shape)
+print(y_data.shape)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
+
+# 모델링 
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, Reshape, Input, Concatenate
+
+input1 = Input(shape=(x_row,x_col))
+layer1 = Conv1D(32,3,activation='relu',padding='same',strides=1)(input1)
+layer1 = Conv1D(32,3,activation='relu',padding='same',strides=1)(layer1)
+layer1 = MaxPooling1D(pool_size=2)(layer1)
+layer1 = Dropout(0.2)(layer1)
+layer1 = Conv1D(32,3,activation='relu',padding='same',strides=1)(input1)
+layer1 = Conv1D(32,3,activation='relu',padding='same',strides=1)(layer1)
+layer1 = MaxPooling1D(pool_size=2)(layer1)
+layer1 = Dropout(0.2)(layer1)
+layer1 = Flatten()(layer1)
+layer1 = Dense(64, activation='relu')(layer1)
+layer1 = Dense(64, activation='relu')(layer1)
+output1 = Dense(96)(layer1)
+
+model = Model(inputs=input1,outputs=output1)
+
+model.summary()
+
+model.compile(loss='mse', optimizer='adam')
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+es = EarlyStopping(monitor='val_loss', patience=50, mode='auto')
+modelpath = "./dacon/data/sunlight_model_binary.hdf5"
+cp = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=5, factor=0.5, verbose=1)
+model.fit(x_train, y_train, epochs=1000, batch_size=256, validation_split=0.2, verbose=2, callbacks=[es,cp,reduce_lr])
+
+loss = model.evaluate(x_test, y_test, batch_size=256)
+print('loss :', loss)
+
+'''
+from tensorflow.keras.models import load_model
+model = load_model('./dacon/data/sunlight_model_binary.hdf5')
+
+submission = pd.DataFrame(np.zeros((7776,9)))
+quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+for i in range(81):
+    # a = []
+    data = pd.read_csv("./dacon/data/test/{}.csv".format(i), header=0).set_index(['Day','Hour','Minute'])
+    for j, k in enumerate(quantiles):
+        a = []
+        a.append(np.array(data.iloc[:,-1:]))
+        x_test = np.array(a)
+        y_pred = model.predict(x_test)
+        # print(y_pred)
+        submission.iloc[96*i:96*(i+1), j] = np.array(y_pred)
+
+
+submission.to_csv('./dacon/data/sample_submission_binary.csv')
+'''
--- dacon/sunlight_submission.py
+++ dacon/sunlight_submission.py
@@ -1,54 +1,62 @@
-# import numpy as np
-# import pandas as pd
-
-# from tensorflow.keras.models import load_model
-# model = load_model('./dacon/data/sunlight_model.hdf5')
-
-# submission = pd.DataFrame(np.zeros((7776,9)))
-# submission.columns = pd.read_csv('./dacon/data/sample_submission.csv', index_col=0, header=0).columns
-# submission.index = pd.read_csv('./dacon/data/sample_submission.csv', index_col=0, header=0).index
-# print(submission)
-# '''
-# import re
-# # print(list(submission.index))
-# # print(list(re.findall('[0-9]+',i[2:]) for i in list(submission.index)))
-# submission.iloc[:,0:3] = np.array(pd.DataFrame(re.findall('[0-9]+',i[2:]) for i in list(submission.index)))
-# '''
-
-# quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-# for i in range(81):
-#     # a = []
-#     data = pd.read_csv("./dacon/data/test/{}.csv".format(i), header=0).set_index(['Day','Hour','Minute'])
-#     for j, k in enumerate(quantiles):
-#         a = []
-#         a.append(np.array(data))
-#         x_test = np.array(a)
-#         y_pred = model.predict(x_test)
-#         submission.iloc[96*i:96*(i+1), j] = np.array(y_pred)
-
-# print(submission)
-# print(submission.shape)
-
-# submission.to_csv('./dacon/data/sample_submission_new.csv')
-
-
-# # 분포 확인
-# import matplotlib.pyplot as plt
-# plt.plot(submission.iloc[:,-1][:192])
-# plt.grid()
-# plt.show()
-
-
-
-# Quantile 해결
 import numpy as np
 import pandas as pd
 
-quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+from tensorflow.keras.models import load_model
+model_2 = load_model('./dacon/data/sunlight_model_binary.hdf5')
 
-df = pd.DataFrame(np.array([[1, 1], [2, 10], [3, 100], [4, 100], [5, 6], [6, 8], [7,10], [8, 11], [9,12], [10 ,11]]),
-                  columns=['a', 'b'])
-df.quantile(.1)
-print(df)
-print()
-print(df.quantile(0.1, interpolation='linear'))
+submission = pd.DataFrame(np.zeros((7776,9)))
+submission.columns = pd.read_csv('./dacon/data/sample_submission.csv', index_col=0, header=0).columns
+submission.index = pd.read_csv('./dacon/data/sample_submission.csv', index_col=0, header=0).index
+print(submission)
+
+quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+for i in range(81):
+    # a = []
+    data = pd.read_csv("./dacon/data/test/{}.csv".format(i), header=0).set_index(['Day','Hour','Minute'])
+    for j, k in enumerate(quantiles):
+        model_1 = load_model('./dacon/data/sunlight_model_{}_qauntile{}.hdf5'.format(j+1,k), compile=False)
+        a = []
+        a.append(np.array(data))
+        x_test = np.array(a)
+        y_pred_1 = model_1.predict(x_test)
+        a = []
+        a.append(np.array(data.iloc[:,-1:]))
+        x_test = np.array(a)
+        y_pred_2 = model_2.predict(x_test)
+        y_pred_2 = np.array([1 if x > 0.5 else 0 for x in (y_pred_2).reshape(96)])
+        submission.iloc[96*i:96*(i+1), j] = np.array([x for x in (y_pred_1*y_pred_2).reshape(96)])
+        # submission.iloc[96*i:96*(i+1), j] = np.array([x if x > 1 else 0 for x in (y_pred_1*y_pred_2).reshape(96)])
+
+
+print(submission)
+print(submission.shape)
+
+submission.to_csv('./dacon/data/sample_submission_new.csv')
+
+'''
+# 원본
+quantiles = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+for i in range(81):
+    # a = []
+    data = pd.read_csv("./dacon/data/test/{}.csv".format(i), header=0).set_index(['Day','Hour','Minute'])
+    for j, k in enumerate(quantiles):
+        a = []
+        a.append(np.array(data))
+        x_test = np.array(a)
+        y_pred = model.predict(x_test)
+        submission.iloc[96*i:96*(i+1), j] = np.array(y_pred)
+
+print(submission)
+print(submission.shape)
+
+submission.to_csv('./dacon/data/sample_submission_new.csv')
+'''
+
+'''
+# 분포 확인
+submission = pd.read_csv('./dacon/data/sample_submission_new.csv', index_col=0, header=0)
+import matplotlib.pyplot as plt
+plt.plot(submission.iloc[:,-1][:192])
+plt.grid()
+plt.show()
+'''
