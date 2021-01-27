38992415e8e82e84771d564b573d86e638b243dc
Taerimmm <xofla7560@naver.com>
2021-01-27 20:57:18 +0900

    Classifier_all_estimators

---
+++ ml/m09_selectModel_3_wine.py
@@ -0,0 +1,42 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.utils import all_estimators
+from sklearn.datasets import load_wine
+import warnings
+
+warnings.filterwarnings('ignore')
+
+dataset = load_wine()
+x = dataset.data
+y = dataset.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=44)
+
+import sklearn
+print(sklearn.__version__) # 0.23.2
+
+# all_estimators -> 0.20 에 최적화되어있다.
+
+allAlgorithms = all_estimators(type_filter='classifier') # sklearn의 분류형 모델 전체
+# print(allAlgorithms)
+
+for (name, algorithm) in allAlgorithms:
+    try :
+        model = algorithm()
+
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        print(name, '의 정답율 :', accuracy_score(y_test, y_pred))
+    except :
+        # continue
+        print(name, '은 없는 놈') # 0.23.2 에 없는 algorithm
+
+# 기준이 되는 지표로 삼을 수 있다.
+'''
+# Max-Score Algorithm
+QuadraticDiscriminantAnalysis 의 정답율 : 1.0
+'''
+
+'''
+Tensorflow's acc : 0.9722222089767456
+'''
