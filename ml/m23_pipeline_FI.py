981e38fab4ad24ec6ad1ad18eae4314cca1667d0
Taerimmm <xofla7560@naver.com>
2021-02-01 18:58:29 +0900

    pipeline_FI

--- ml/m23_pipeline_FI.py
+++ ml/m23_pipeline_FI.py
@@ -25,33 +25,40 @@ print(x_test.shape, y_test.shape)
 
 kfold = KFold(n_splits=5, shuffle=True)
 
-parameters = [{'randomforestregressor__n_estimators':[100,150],
-               'randomforestregressor__max_depth':[2,4],
-               'randomforestregressor__min_samples_split':[2,4],
-               'randomforestregressor__min_samples_leaf':[1,3],
+parameters = [{'randomforestregressor__n_estimators':[100,150,200,250,300],
+               'randomforestregressor__max_depth':[2,4,6,8],
+               'randomforestregressor__min_samples_split':[2,4,6,8],
+               'randomforestregressor__min_samples_leaf':[1,3,5,7],
                'randomforestregressor__n_jobs':[-1]}]
 
 pipe = Pipeline([('MinMax',MinMaxScaler()), ('randomforestregressor',RandomForestRegressor())])
 
-model = GridSearchCV(pipe, parameters, cv=kfold)
+model = RandomizedSearchCV(pipe, parameters, cv=kfold)
 
 model.fit(x_train, y_train)
 
-print(model.score(x_test, y_test))
+acc = model.score(x_test, y_test)
+
+print('acc :', acc)
 
 print('best_estimator :', model.best_estimator_)
 
 # pipeline 에서 feature_importances_ ??
 
-'''
+print(model.best_estimator_.steps)
+best_estimator = model.best_estimator_.steps[1][1]
+
+print(best_estimator.feature_importances_)
+
+
+
 def plot_feature_importances_dataset(model):
     n_features = x_train.shape[1]
     plt.barh(np.arange(n_features), model.feature_importances_, align='center')
-    plt.yticks(np.arange(n_features), x_train.feature_names)
+    # plt.yticks(np.arange(n_features), dataset.feature_names)
     plt.xlabel('Feature Importances')
     plt.ylabel('Features')
     plt.ylim(-1, n_features)
 
-plot_feature_importances_dataset(best_estimator)
+plot_feature_importances_dataset(model.best_estimator_.steps[1][1])
 plt.show()
-'''
