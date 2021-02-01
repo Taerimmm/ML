001be6c654a82e71437b6d62604c218352470ed6
Taerimmm <xofla7560@naver.com>
2021-02-01 19:10:17 +0900

    plot_importance

---
+++ ml/m25_XGB_plot_importance3_wine.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+from sklearn.datasets import load_wine
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline, make_pipeline
+
+# cmd -> pip install xgboost
+from xgboost import XGBClassifier, plot_importance
+
+# 1. 데이터
+dataset = load_wine()
+
+x_train, x_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.2, random_state=44)
+
+# 2. 모델
+model = XGBClassifier(n_jobs=-1)
+
+# 3. 훈련
+model.fit(x_train, y_train)
+
+# 4. 평가, 예측
+acc = model.score(x_test, y_test)
+
+print(model.feature_importances_)
+print('acc :', acc)
+
+'''
+def plot_feature_importances_dataset(model):
+    n_features = dataset.data.shape[1]
+    plt.barh(np.arange(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), dataset.feature_names)
+    plt.xlabel('Feature Importances')
+    plt.ylabel('Features')
+    plt.ylim(-1, n_features)
+
+plot_feature_importances_dataset(model)
+'''
+plot_importance(model)
+plt.show()
