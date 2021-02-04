94bf4d8dfcff926a98b8b86b698f0cfb0c3ce354
Taerimmm <xofla7560@naver.com>
2021-02-04 11:38:16 +0900

    Dacon_vision

--- dacon2/vision_preprocessing.py
+++ dacon2/vision_preprocessing.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 
-from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+from sklearn.model_selection import train_test_split, StratifiedKFold
 
-from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Activation
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
@@ -45,11 +45,13 @@ print(x_train.shape, y_train.shape)     # (2048, 28, 28, 1) (2048, 10)
 
 datagen = ImageDataGenerator(
     rotation_range=10,
-    zoom_range=0.1,
     width_shift_range=0.1,  
     height_shift_range=0.1)
 
-skfold = StratifiedKFold(n_splits=10, shuffle=True)
+datagen2 = ImageDataGenerator()
+
+steps = 20
+skfold = StratifiedKFold(n_splits=steps, shuffle=True)
 
 # 2. 모델
 def cnn_model(x_train):
@@ -183,20 +185,25 @@ for i, (train_idx, val_idx) in enumerate(skfold.split(x_train, y_train.argmax(1)
     model = cnn_model2(x_train)
 
     filepath = './dacon2/data/vision_model_{}.hdf5'.format(i)
-    es = EarlyStopping(monitor='val_accuracy', patience=50, mode='auto')
+    es = EarlyStopping(monitor='val_accuracy', patience=200, mode='auto')
     cp = ModelCheckpoint(filepath=filepath, monitor='val_accuracy', save_best_only=True, mode='auto')
-    lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.25, patience=20)
+    lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=100)
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    hist = model.fit_generator(datagen.flow(x_train_, y_train_, batch_size=32), epochs=1000,
-               validation_data=(datagen.flow(x_val_, y_val_, batch_size=32)), verbose=2, callbacks=[es, cp, lr])
+    hist = model.fit_generator(datagen.flow(x_train_, y_train_, batch_size=32), epochs=2000,
+               validation_data=(datagen2.flow(x_val_, y_val_)), verbose=2, callbacks=[es, cp, lr])
 
     val_acc.append(max(hist.history['val_accuracy']))
 
     print('{}\'s CV End'.format(i+1))
 
 # 3. 예측
+test_data = pd.read_csv('./dacon2/data/test.csv', index_col=0, header=0)
+x_test = test_data.drop(['letter'], axis=1).values
+x_test = x_test.reshape(-1, 28, 28, 1)
+x_test = x_test/255
 
+# best model select
 print(val_acc)
 
 i_max = np.argmax(val_acc)
@@ -204,19 +211,26 @@ i_max = np.argmax(val_acc)
 print('Best Model is {}\'s'.format(i_max))
 model = load_model('./dacon2/data/vision_model_{}.hdf5'.format(i_max))
 
-test_data = pd.read_csv('./dacon2/data/test.csv', index_col=0, header=0)
-x_test = test_data.drop(['letter'], axis=1).values
-x_test = x_test.reshape(-1, 28, 28, 1)
-x_test = x_test/255
-
 submission = pd.read_csv('./dacon2/data/submission.csv', index_col=0, header=0)
 
 submission['digit'] = np.argmax(model.predict(x_test), axis=1)
 print(submission)
 
-submission.to_csv('./dacon2/data/submission_test.csv')
+submission.to_csv('./dacon2/data/submission_model_best.csv')
 
 
+# KFold 값 평균내기
+submission2 = pd.read_csv('./dacon2/data/submission.csv', index_col=0, header=0)
+
+result = 0
+for i in range(steps):
+    model = load_model('./dacon2/data/vision_model_{}.hdf5'.format(i))
+
+    result += model.predict_generator(datagen2.flow(x_test, shuffle=False)) / steps
+    submission2['digit'] = result.argmax(1)
+
+print(submission2)
+submission2.to_csv('./dacon2/data/submission_model_mean.csv')
 
 
 
