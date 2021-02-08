c21ae52f5e65b1527fbb3c834abca2bd5cc02ae7
Taerimmm <xofla7560@naver.com>
2021-02-08 23:08:10 +0900

    Dacon_vision2

--- dacon3/2_appetizer.py
+++ dacon3/2_appetizer.py
@@ -6,7 +6,6 @@ import matplotlib.image as mpimg
 from PIL import Image
 import cv2
 
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import KFold
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Input, Dense, Conv2D, BatchNormalization, Concatenate, ReLU
@@ -51,7 +50,7 @@ y_train = y_train[:size]
 print(y_train)
 print(y_train.shape)
 
-steps = 20
+steps = 2
 kfold = KFold(n_splits=steps, random_state=45, shuffle=True)
 
 # 2. 모델
@@ -106,27 +105,114 @@ def cnn_model(x_train, dropout_rate=0.3, growth_rate=32):
 
 
 # 3. 훈련
+def macro_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost.
+    Average (1 - soft-F1) across all labels.
+    Use probability values instead of binary predictions.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    soft_f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    cost = 1 - soft_f1 # reduce 1 - soft-f1 in order to increase soft-f1
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    
+    return macro_cost
+
+def macro_f1(y, y_hat, thresh=0.5):
+    """Compute the macro F1-score on a batch of observations (average F1 across labels)
+    
+    Args:
+        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        thresh: probability value above which we predict positive
+        
+    Returns:
+        macro_f1 (scalar Tensor): value of macro F1 for the batch
+    """
+    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
+    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
+    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
+    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
+    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    macro_f1 = tf.reduce_mean(f1)
+    return macro_f1
+
+
+
 for i, (train_idx, val_idx) in enumerate(kfold.split(x_train, y_train)):
     x_train_, x_val_ = x_train[train_idx], x_train[val_idx]
     y_train_, y_val_ = y_train.iloc[train_idx,:], y_train.iloc[val_idx,:]
     
     model = cnn_model(x_train)
 
-    model.summary()
+    # model.summary()
 
     filepath = './dacon3/data/vision_2_model_{}.hdf5'.format(i)
     es = EarlyStopping(monitor='val_loss', patience=160, mode='auto')
     cp = ModelCheckpoint(filepath=filepath, monitor='val_loss', save_best_only=True, mode='auto')
     lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=100)
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(x_train_, y_train_, epochs=2000, batch_size=32, validation_data=(x_val_, y_val_), verbose=2, callbacks=[es, cp, lr])
+    model.compile(loss=macro_soft_f1, optimizer='adam', metrics=[macro_f1])
+    model.fit(x_train_, y_train_, epochs=20, batch_size=32, validation_data=(x_val_, y_val_), verbose=2, callbacks=[es, cp, lr])
 
     print('{}\'s CV End'.format(i+1))
 '''
 # 4. 예측
+
+def macro_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost.
+    Average (1 - soft-F1) across all labels.
+    Use probability values instead of binary predictions.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    soft_f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    cost = 1 - soft_f1 # reduce 1 - soft-f1 in order to increase soft-f1
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    
+    return macro_cost
+
+def macro_f1(y, y_hat, thresh=0.5):
+    """Compute the macro F1-score on a batch of observations (average F1 across labels)
+    
+    Args:
+        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        thresh: probability value above which we predict positive
+        
+    Returns:
+        macro_f1 (scalar Tensor): value of macro F1 for the batch
+    """
+    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
+    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
+    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
+    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
+    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    macro_f1 = tf.reduce_mean(f1)
+    return macro_f1
+
 a = []
-size = 50
+size = 1
 for i in range(size):
     img = cv2.imread('../Data/test_dirty_mnist_2nd/{0:05d}.png'.format(i+50000))
 
@@ -144,13 +230,14 @@ print(x_test.shape)
 # KFold 값 평균내기
 submission = pd.read_csv('./dacon3/data/sample_submission.csv', index_col=0, header=0)
 print(submission)
-'''
+
 result = 0
+steps = 2
 for i in range(steps):
-    model = load_model('./dacon2/data/vision_2_model_{}.hdf5'.format(i))
+    model = load_model('./dacon3/data/vision_2_model_{}.hdf5'.format(i), custom_objects={'macro_soft_f1':macro_soft_f1, 'macro_f1':macro_f1})
 
-    result += model.predict(x_test) / steps
+    result += model.predict(x_test)
+    print(result)
 
-print(submission)
-submission.to_csv('./dacon3/data/vision_2_submission.csv')
-'''
+# print(submission)
+# submission.to_csv('./dacon3/data/vision_2_submission.csv')
--- dacon3/3_merge_set.py
+++ dacon3/3_merge_set.py
@@ -1,38 +1,59 @@
 import numpy as np
 import cv2
 
-# X_Train - numpy 
-a = []
-size = 50
-for i in range(size):
-    img = cv2.imread('../Data/dirty_mnist_2nd/{0:05d}.png'.format(i))
+# # X_Train - numpy 
+# a = []
+# b = []
+# size = 50000
+# for i in range(size):
+#     img = cv2.imread('../Data/dirty_mnist_2nd/{0:05d}.png'.format(i), cv2.IMREAD_GRAYSCALE)
 
-    denoised_img = cv2.fastNlMeansDenoising(img, None, 30, 15, 21)
+#     denoised_img = cv2.fastNlMeansDenoising(img, None, 30, 15, 21)
 
-    a.append(denoised_img)
+#     a.append(img)
+#     b.append(denoised_img)
 
-x_train = np.array(a)
-x_train = x_train / 255
-print(x_train)
-print(type(x_train))
-print(x_train.shape)
+# x_train = np.array(a)
+# x_train = x_train / 255
+# x_train_denoise = np.array(b)
+# x_train_denoise = x_train_denoise / 255
 
-np.save('./dacon3/data/x_train_merge.npy', x_train)
+# print(x_train)
+# print(type(x_train))
+# print(x_train.shape)
+
+# print(x_train_denoise)
+# print(type(x_train_denoise))
+# print(x_train_denoise.shape)
+
+# np.save('./dacon3/data/x_train_merge.npy', x_train)
+# np.save('./dacon3/data/x_train_denoise_merge.npy', x_train_denoise)
 
 # X_Test - numpy
 a = []
-size = 50
+b = []
+size = 5000
 for i in range(size):
-    img = cv2.imread('../Data/test_dirty_mnist_2nd/{0:05d}.png'.format(i+50000))
-
+    img = cv2.imread('../Data/test_dirty_mnist_2nd/{0:05d}.png'.format(i+50000), cv2.IMREAD_GRAYSCALE)
     denoised_img = cv2.fastNlMeansDenoising(img, None, 30, 15, 21)
 
-    a.append(denoised_img)
+    img = img / 255
+    denoised_img = denoised_img / 255
+
+    a.append(img)
+    b.append(denoised_img)
 
 x_test = np.array(a)
-x_test = x_test / 255
+x_test_denoise = np.array(b)
+
 print(x_test)
 print(type(x_test))
 print(x_test.shape)
 
+print(x_test_denoise)
+print(type(x_test_denoise))
+print(x_test_denoise.shape)
+
+
 np.save('./dacon3/data/x_test_merge.npy', x_test)
+np.save('./dacon3/data/x_test_denoise_merge.npy', x_test_denoise)
