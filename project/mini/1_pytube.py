2ae55bb134f2dc3048f145c9dad2440820450071
Taerimmm <xofla7560@naver.com>
2021-02-14 19:02:05 +0900

    projecty

---
+++ project/mini/1_pytube.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+import glob
+import os.path
+
+# 유튜브 전용 인스턴스 생성
+par = 'https://youtu.be/0-q1KafFCLU'
+yt = YouTube(par)
+yt.streams.filter(only_audio=True).all()
+
+# 특정영상 다운로드
+yt.streams.filter(only_audio=True).first().download('./project/mini/data', filename='1') # filename 수정해서 원하는 file명으로 고치기.
+print('success')
+
+# 확장자 변경
+os.chdir('./project/mini/data')
+files = glob.glob("*.mp4")
+for x in files:
+    if not os.path.isdir(x):
+        filename = os.path.splitext(x)
+        try:
+            os.rename(x, filename[0] + '.mp3')
+        except:
+            pass
---
+++ project/mini/2_demo.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
+import warnings
+warnings.filterwarnings('ignore')
+
+y, sr = librosa.load('./project/mini/data/0.mp3')
+
+print(y.shape)
+print(sr)
+
+plt.plot(y)
+plt.title('Signal')
+plt.xlabel('Time (samples)')
+plt.ylabel('Amplitude')
+plt.show()
+
+# fft - fast Fourier transform
+n_fft = 2048
+ft = np.abs(librosa.stft(y[:n_fft], hop_length = n_fft+1))
+
+plt.plot(ft)
+plt.title('spectrum')
+plt.xlabel('Frequency Bin')
+plt.ylabel('Amplitude')
+plt.show()
+
+spec = np.abs(librosa.stft(y, hop_length=512))
+spec = librosa.amplitude_to_db(spec, ref=np.max) # converting to decibals
+
+# Plotting the spectrogram
+plt.figure(figsize=(8,5))
+librosa.display.specshow(spec, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Spectrogram')
+plt.show()
+
+
+# Computing the mel spectrogram
+spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
+spect = librosa.power_to_db(spect, ref=np.max) # Converting to decibals
+
+# Plotting the mel spectrogram
+plt.figure(figsize=(8,5))
+librosa.display.specshow(spect, y_axis='mel', fmax=8000, x_axis='time')
+plt.title('Mel Spectrogram')
+plt.colorbar(format='%+2.0f dB')
+plt.show()
+
+
+''' Mel Frequency Cepstral Coefficients (MFCC) '''
+
+# Extracting mfccs from the audio signal
+mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=512, n_mfcc=13)
+
+# Displaying the mfccs
+plt.figure(figsize=(8,5))
+librosa.display.specshow(mfcc, x_axis='time')
+plt.title('MFCC')
+plt.show()
+
+mfccscaled = np.mean(mfcc.T, axis=0)
+print(mfccscaled)
