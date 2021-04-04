346900674b8a4ef582f5604bbdd39431c394f584
Taerimmm <xofla7560@naver.com>
2021-04-04 23:05:48 +0900

    Project

--- project/team/picture.py
+++ project/team/picture.py
@@ -1,8 +1,5 @@
-import cv2
 import os
-import numpy as np
-import pandas as pd
-from PIL import Image
+import cv2
 
 for file in os.scandir('./project/team/filtered_img'):
     print(file)
@@ -19,21 +16,3 @@ for file in os.scandir('./project/team/filtered_img'):
 
     # print('./project/team/resize_img/'+path[35:])    
     cv2.imwrite('./project/team/resize_img/' + path[35:], jpg)
-
-
-
-# jpg_1 = cv2.imread('481_pre.jpg')
-# jpg_1 = cv2.resize(jpg_1, (1280,720))
-# jpg = cv2.imread('481.jpg')
-# print(jpg.shape)
-# re_jpg = cv2.resize(jpg, (1280,720))
-# print(re_jpg.shape)
-
-
-# cv2.imshow('jpg_1', jpg_1)
-# cv2.imshow('re_jpg', re_jpg)
-
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# cv2.imwrite()
