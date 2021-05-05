6c8a25b4844cf7d02b1aa631f13fb2716022691b
Taerimmm <xofla7560@naver.com>
2021-05-05 23:48:53 +0900

    Autokeras

--- keras3/keras107_boston.py
+++ keras3/keras107_boston.py
@@ -6,6 +6,9 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+from tensorflow.keras.models import load_model
+from sklearn.metrics import r2_score
+
 dataset = load_boston()
 x = dataset.data
 y = dataset.target
@@ -26,3 +29,18 @@ model.fit(x_train, y_train, epochs=10)
 
 result = model.evaluate(x_test, y_test)
 print(result)
+
+
+model2 = model.export_model()
+try:
+    model2.save('ak_save_boston', save_format='tf')
+except:
+    model2.save('ak_save_boston.h5')
+
+model3 = load_model('ak_save_boston', custom_objects=ak.CUSTOM_OBJECTS)
+result_boston = model3.evaluate(x_test, y_test)
+
+y_pred = model3.predict(x_test)
+r2 = r2_score(y_test, y_pred)
+
+print("load_result :", result_boston, r2)
--- keras3/keras108_cancer.py
+++ keras3/keras108_cancer.py
@@ -5,6 +5,9 @@ import autokeras as ak
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 
+from tensorflow.keras.models import load_model
+from sklearn.metrics import accuracy_score
+
 dataset = load_breast_cancer()
 x = dataset.data
 y = dataset.target
@@ -20,3 +23,17 @@ model.fit(x_train, y_train, epochs=10)
 
 result = model.evaluate(x_test, y_test)
 print(result)
+
+model2 = model.export_model()
+try:
+    model2.save('ak_save_boston', save_format='tf')
+except:
+    model2.save('ak_save_boston.h5')
+
+model3 = load_model('ak_save_boston', custom_objects=ak.CUSTOM_OBJECTS)
+result_boston = model3.evaluate(x_test, y_test)
+
+y_pred = model3.predict(x_test)
+acc = accuracy_score(y_test, y_pred)
+
+print("load_result :", result_boston, acc)
